d7ec52f3c07db3c8db64b0689ece06b4ae347a8a
PETIT OLIVIER p1613946 <olivier.petit007@orange.fr>
2017-05-03 11:03:26 +0200

    correction de l'étape 2

--- src/main/java/edu/lyon1/RootController.java
+++ src/main/java/edu/lyon1/RootController.java
@@ -2,6 +2,7 @@ package edu.lyon1;
 
 import javax.servlet.http.HttpServletRequest;
 import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.RequestHeader;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.servlet.ModelAndView;
 
@@ -9,13 +10,15 @@ import java.util.*;
 
 @Controller
 @RequestMapping("/")
+
 public class RootController {
 
     @RequestMapping("/")
-    public ModelAndView test(HttpServletRequest request) {
+    public ModelAndView test(HttpServletRequest request, @RequestHeader Map<String,String> headers){
         ModelAndView mav = new ModelAndView();
         mav.addObject("titre", "IUT");
         mav.addObject("corps", "bonjour");
+        mav.addObject("headers_etape2",headers.keySet());
 
         mav.setViewName("template");
          List<String> listHeader = Collections.list(request.getHeaderNames());
