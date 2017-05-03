bc92d4cc3d8b89bfdebb9b16ddf2ebfa204014a8
PETIT OLIVIER p1613946 <olivier.petit007@orange.fr>
2017-05-03 11:47:13 +0200

    correction de l'étape 3

--- src/main/java/edu/lyon1/RootController.java
+++ src/main/java/edu/lyon1/RootController.java
@@ -1,9 +1,12 @@
 package edu.lyon1;
 
 import javax.servlet.http.HttpServletRequest;
+
+import org.springframework.http.HttpHeaders;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.RequestHeader;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestMethod;
 import org.springframework.web.servlet.ModelAndView;
 
 import java.util.*;
@@ -14,20 +17,54 @@ import java.util.*;
 public class RootController {
 
     @RequestMapping("/")
-    public ModelAndView test(HttpServletRequest request, @RequestHeader Map<String,String> headers){
+    public ModelAndView test(HttpServletRequest request, @RequestHeader HttpHeaders headers){
         ModelAndView mav = new ModelAndView();
         mav.addObject("titre", "IUT");
         mav.addObject("corps", "bonjour");
         mav.addObject("headers_etape2",headers.keySet());
 
         mav.setViewName("template");
-         List<String> listHeader = Collections.list(request.getHeaderNames());
+         List<HttpHeader> listHeader = new ArrayList<>();
+         HttpHeader toto = new HttpHeader("value","name");
+                        mav.addObject("htpheader", toto.getValue());
+        for (Map.Entry<String, List<String>> entry : headers.entrySet()) {
+            String values = String.join(",", entry.getValue());
+            listHeader.add(new HttpHeader(values,entry.getKey()));
+        }
 
-         mav.addObject("headers",listHeader);
-
-
+        mav.addObject("headers",listHeader);
         return mav;
     }
+public class HttpHeader{
+
+    public String getValue() {
+        return value;
+    }
+
+    public void setValue(String value) {
+        this.value = value;
+    }
+
+    String value;
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    String name;
+
+    public HttpHeader(String value, String name) {
+        this.value = value;
+        this.name = name;
+
+    }
+}
+
+
 
 }
 
